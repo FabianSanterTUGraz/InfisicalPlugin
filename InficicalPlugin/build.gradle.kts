4308158dc657a19c49902448865816042f2e8b22
Fabian Santer <fabian.santer@abuscom.com>
2026-07-15 15:24:59 +0200

    Add Gradle plugin and JUnit4 dependencies, fix Windows build
    bundledPlugin("com.intellij.gradle") gives access to
    GradleRunConfiguration/ExternalSystemTaskExecutionSettings for the
    secret-injection scaffolding; junit4 is required transitively by the
    IntelliJ Platform test framework (TestFrameworkType.Platform);
    instrumentCode=false works around the legacy Ant instrumentation
    (Javac2) failing on Windows JDKs that lack a "Packages" subfolder.

--- InficicalPlugin/build.gradle.kts
+++ InficicalPlugin/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     testImplementation(libs.junit.jupiter)
+    testImplementation(libs.junit4)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
@@ -18,9 +19,16 @@ dependencies {
 
         // Add plugin dependencies for compilation here:
         bundledPlugin("com.intellij.modules.json")
+        bundledPlugin("com.intellij.gradle")
     }
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+// Deaktiviert, da die Legacy-Ant-Instrumentierung (Javac2) unter Windows-JDKs (z.B. .jdks/ms-25.0.3)
+// einen nicht existierenden "Packages"-Unterordner erwartet und mit einem Build-Fehler abbricht.
+intellijPlatform {
+    instrumentCode = false
 }

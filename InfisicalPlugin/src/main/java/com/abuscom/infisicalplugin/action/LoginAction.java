5e740da839a3b144b37584f2f170b7d0f267bbe9
Fabian Santer <fabian.santer@abuscom.com>
2026-07-21 12:42:41 +0200

    Turn Login action into a logout action
    Browser-based login is already triggered from the run config settings
    button, making this action's original login flow redundant. Repurpose
    it to clear the stored token via TokenManager.clearKeypass(), and drop
    the now-dead login URL/callback-port code.

--- InfisicalPlugin/src/main/java/com/abuscom/infisicalplugin/action/LoginAction.java
+++ InfisicalPlugin/src/main/java/com/abuscom/infisicalplugin/action/LoginAction.java
@@ -1,48 +1,25 @@
 package com.abuscom.infisicalplugin.action;
 
-import com.abuscom.infisicalplugin.infisical.http.InfisicalHttpClient;
-import com.abuscom.infisicalplugin.infisical.login.LoginCallBackServer;
-import com.abuscom.infisicalplugin.infisical.login.TokenChangeListener;
 import com.abuscom.infisicalplugin.infisical.login.TokenManager;
-import com.intellij.ide.BrowserUtil;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-
 public class LoginAction extends AnAction {
-    public static final int LOGIN_CALLBACK_PORT = 8010;
 
     public LoginAction() {
-        super("Login", "Mit Infisical einloggen", AllIcons.Actions.Forward);
+        super("Logout", "Infisical-Token entfernen", AllIcons.Actions.Exit);
     }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        LoginCallBackServer server = new LoginCallBackServer(TokenManager.getInstance());
-        TokenManager.getInstance().addTokenChangeListener(new TokenChangeListener() {
-            @Override
-            public void onTokenChanged(String token) {
-                TokenManager.getInstance().removeTokenChangeListener(this);
-            }
-        });
-        try {
-            server.startServer();
-            BrowserUtil.browse(buildLoginUrl());
-        } catch (IOException ex) {
-            System.err.println("Callback server exception: " + ex.getMessage());
-        }
+        TokenManager.getInstance().clearKeypass();
     }
 
     @Override
     public @NotNull ActionUpdateThread getActionUpdateThread() {
         return ActionUpdateThread.BGT;
-    }
-
-    private static String buildLoginUrl() {
-        return InfisicalHttpClient.DEFAULT_BASE_URL + "/login?callback_port=" + LOGIN_CALLBACK_PORT;
     }
 }
